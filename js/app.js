4abea90a6b4c6da5e98bc0ce393ad41260e7afc1
kumarram27 <kumarram59266@gmail.com>
2024-01-19 00:57:42 +0530

    commit changes

--- js/app.js
+++ js/app.js
@@ -1,1 +1,17 @@
 "use strict";
+document.addEventListener("DOMContentLoaded", function () {
+  var copyBtn = document.querySelector(".copy_btn");
+  var copyText = document.querySelector(".copy_text");
+
+  copyBtn.addEventListener("click", function () {
+    // Your copy logic goes here
+    // For now, let's just update the copy_text content
+    copyText.textContent = "Copied!!";
+
+    // Set a timeout to make the "Copied" text disappear after 2000 milliseconds (2 seconds)
+    setTimeout(function () {
+      copyText.textContent = "";
+    }, 250);
+  });
+});
+
